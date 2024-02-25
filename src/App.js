48e14068bc8ae4e6f11fe8e9c4ca7ac8f1b9aeb7
Praynat <80518903+Praynat@users.noreply.github.com>
2024-02-25 21:19:02 +0200

    React_Training_Components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+import { Box, Typography, Button } from '@mui/material';
 import './App.css';
+import MyComponent from './Components/MyComponent';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const myStyle ={
+  display:"flex",
+  flexDirection:"column",
+  justifyContent:"center", 
+  alignItems:"center",
+  width:"100vw",
+  height:"100vh",
+  backgroundColor:"beige"
+}
+
+
+  return  <Box> 
+  <Box sx={myStyle}>
+    <MyComponent></MyComponent>
+      <Typography variant='h3'>Hello</Typography>
+      <Button variant="contained" color="primary">
+        Click Me
+      </Button>
+  </Box>
+
+  <Box sx={myStyle}>
+    
+  </Box>
+    
+    </Box>
+  // <div style={myStyle}>
+    
+  //   <MyComponent></MyComponent>
+  
+  // </div>
+  // return (
+    
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React Now
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/Components/MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function MyComponent() {
+  return (
+    <div>
+        <h1 style={{fontSize:"5rem"}}>
+            Wow! It is amazing!
+        </h1>
+    </div>
+  )
+}
+
+export default MyComponent
